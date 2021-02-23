dc1415330730589f4ee83e7a573234fc40fdcb48
ferdie <ferdiebergado@gmail.com>
2021-02-23 14:57:31 +0800

    Added cmus

---
+++ dot_config/private_cmus/executable_cmus-status-wayland.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+#
+# cmus-status-display
+#
+# Usage:
+#   in cmus command ":set status_display_program=cmus-status-display"
+#
+# This scripts is executed by cmus when status changes:
+#   cmus-status-display key1 val1 key2 val2 ...
+#
+# All keys contain only chars a-z. Values are UTF-8 strings.
+#
+# Keys: status file url artist album discnumber tracknumber title date
+#   - status (stopped, playing, paused) is always given
+#   - file or url is given only if track is 'loaded' in cmus
+#   - other keys/values are given only if they are available
+#
+
+# output() {
+# write status to ~/cmus-status.txt (not very useful though)
+# echo "*: $*" >>~/cmus-status.txt 2>&1
+# ICON=~/Music/cover.png
+
+# dunstify -r 1996 -a cmus -i ~/Music/cover.png "cmus" "$*"
+
+# WMI (http://wmi.modprobe.de/)
+#wmiremote -t "$*" &> /dev/null
+# }
+
+# errtrap() {
+#     echo "ERROR: ${BASH_SOURCE[1]} at ${BASH_LINENO[0]}" >>~/cmus-status.txt 2>&1
+# }
+
+# set -o errtrace
+# trap errtrap ERR
+
+while test $# -ge 2; do
+    eval _$1='$2'
+    shift
+    shift
+done
+
+if test "$_status" == "playing"; then
+    if test -n "$_file"; then
+        # TMPDIR=/tmp/eyed3
+        TMPFILE="/tmp/fileinfo.txt"
+        TMPCOVER="/tmp/FRONT_COVER.jpg"
+        # [[ ! -d $TMPDIR ]] && mdkir $TMPDIR
+
+        # Save metadata and cover art to a temporary directory
+        eyeD3 --write-images /tmp "$_file" >$TMPFILE
+
+        # Extract metadata from temporary file
+        extract_tag() {
+            output="$(cat $TMPFILE | grep -m 1 $1 | cut -d: -f2)"
+            chars=$(echo "$output" | wc -m)
+
+            # Fallback if current song has no metadata: obtain metadata from the filename
+            if test $chars -le 2; then
+
+                FILE="$(basename "$_file" .mp3)"
+
+                case $1 in
+                    title)
+                        output="$(echo "$FILE" | awk -F ' - ' '{print $1}')"
+                        ;;
+                    artist)
+                        output="$(echo "$FILE" | awk -F ' - ' '{print $2}')"
+                        ;;
+                    album | date)
+                        output=
+                        ;;
+                esac
+                output="$(echo "$output" | tr -d '\\')"
+            fi
+
+            echo "$output"
+        }
+
+        # Metadata
+        title="$(extract_tag title)"
+        artist="$(extract_tag artist)"
+        album="$(extract_tag album)"
+        date="$(extract_tag date | tr -d ' ')"
+        year=
+        [[ -n $date ]] && year="($date)"
+        duration=$(cat -A $TMPFILE | awk '/Time/{print $2}' | sed 's/\^\[\[22m//' | cut -d^ -f1)
+
+        # Default cover art
+        art="$HOME/Music/cover.png"
+        [[ -f $TMPCOVER ]] && art="$TMPCOVER"
+
+        # Check for running wms
+        if [ -n "$(pgrep -x awesome)" ]; then
+            # Notification for awesome wm
+            display="$artist"
+            [[ -n "$album" ]] && display="$display\\\n$album"
+            [[ -n "$year" ]] && display="$display\\\n$year"
+
+            echo -e "notify(\"$art\", \"$title\", \"$display\")" | awesome-client
+        else
+            # Notification for other wms
+            display="$artist\n$album $year\n $duration"
+            notify-send.sh -i "$art" "$title" "$display" --replace-file="/tmp/cmus-notification" &
+        fi
+
+        # Clean up
+        sleep 1
+        [[ -f "$TMPCOVER" ]] && rm -f "$TMPCOVER"
+
+    elif test -n "$_url"; then
+        output "[$_status] $_url - $_title"
+    else
+        output "[$_status]"
+    fi
+fi
---
+++ dot_config/private_cmus/lib.pl
@@ -0,0 +1,67 @@
+/home/d0np0br3/Music/downloads/ chill beats.mp3
+/home/d0np0br3/Music/downloads/ANGELIKA - Spirit of Love.mp3
+/home/d0np0br3/Music/downloads/ATB - Trilogy (The Final Chapter).mp3
+/home/d0np0br3/Music/downloads/Cosmic Ray - Promise.mp3
+/home/d0np0br3/Music/downloads/DJ Nejtrino feat. Elia - Zachem toptat moyu lyubov.mp3
+/home/d0np0br3/Music/downloads/Eden's Bridge - Listen To Our Hearts.mp3
+/home/d0np0br3/Music/downloads/Ennio Morricone feat. Edda Dell'Orso - Once Upon a Time in the West.mp3
+/home/d0np0br3/Music/downloads/Entheo - Endless Wonder (Iridescent Mix).mp3
+/home/d0np0br3/Music/downloads/Gregory Esayan feat. Natalia Pevcova - Into the Night (Original Mix).mp3
+/home/d0np0br3/Music/downloads/Juliana - Lapis Heart.mp3
+/home/d0np0br3/Music/downloads/Koven - Another Home.mp3
+/home/d0np0br3/Music/downloads/Legarden - Pure City Melody #040.mp3
+/home/d0np0br3/Music/downloads/Legarden - Pure City Melody #046.mp3
+/home/d0np0br3/Music/downloads/Lounge Cafe - Don't speak (cover).mp3
+/home/d0np0br3/Music/downloads/MESSIAH Project - Je T'aime (My Heartbeat).mp3
+/home/d0np0br3/Music/downloads/Matt Darey feat. Marcella Woods - Voice Of An Angel (Samuels Brothers Edit).mp3
+/home/d0np0br3/Music/downloads/Mree - In the Kitchen.mp3
+/home/d0np0br3/Music/downloads/RDJ Oleg - Night O Mix 4.mp3
+/home/d0np0br3/Music/downloads/Simon Le Grec - Don't Give Up to Believe.mp3
+/home/d0np0br3/Music/downloads/Sub Orchestra - Spread Love.mp3
+/home/d0np0br3/Music/downloads/Texas - Day After Day.mp3
+/home/d0np0br3/Music/downloads/Valefim planet - Crystal Desire.mp3
+/home/d0np0br3/Music/downloads/tagtest/CJ Ferrum - Believe Me (Original Mix) (Original Mix).mp3
+/home/d0np0br3/Music/downloads/tagtest/Rassolodin - Within Space.mp3
+/home/d0np0br3/Music/downloads/Aleksey Beloozerov & Ange - So Far Away (R.I.B.Chillout Remix).(AGRMusic).mp3
+/home/d0np0br3/Music/downloads/tagtest/Alicks & Alaskan Tapes - This was just a phase; This was what I wanted (Original mix).mp3
+/home/d0np0br3/Music/downloads/Anna Miracles & Germind - Azure Mirages.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_She_Is_The_One.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_The_Sky_for_Everybody.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_God_is_Love.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_The_Last_Spring_of_Mankind.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_Open_Wide_Your_Eyes.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_Going_Deeper.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_Fly_to_Mars_.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_River_of_Life.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_Gravity_to_You.mp3
+/home/d0np0br3/Music/downloads/Apples from Mars/Apples_From_Mars_Make_Love_Not_War_.mp3
+/home/d0np0br3/Music/downloads/Bei Maejor - End Of The Night.mp3
+/home/d0np0br3/Music/downloads/tagtest/Chanel Cole - Break My Back.mp3
+/home/d0np0br3/Music/downloads/David Guetta feat. Sia - She Wolf (Falling to Pieces).mp3
+/home/d0np0br3/Music/downloads/DIONYS feat. Alexandra Milu - We Found Love.mp3
+/home/d0np0br3/Music/downloads/Evolution ft. Jayn Hanna - Walking On Fire (Ambient Mix).mp3
+/home/d0np0br3/Music/downloads/Fousheé - Deep End.mp3
+/home/d0np0br3/Music/downloads/Gemini Rising, Tensnake, Fiora - Stars Come to an End.mp3
+/home/d0np0br3/Music/downloads/Googoosha - Night.mp3
+/home/d0np0br3/Music/downloads/Gregorian Feat. Sisters Of Oz - Once In A Lifetime (Extended Version).mp3
+/home/d0np0br3/Music/downloads/Hi-Gate - I Can Hear Voices (Chilled Mix).mp3
+/home/d0np0br3/Music/downloads/ID3 feat. Jett - Alone.mp3
+/home/d0np0br3/Music/downloads/Kazukii - Lost In Time (feat. Dianna).mp3
+/home/d0np0br3/Music/downloads/Laleh - Live Tommorrow.mp3
+/home/d0np0br3/Music/downloads/Loreen - Heal (feat. Blanks).mp3
+/home/d0np0br3/Music/downloads/Meshach Gordon - A Long Way Down.mp3
+/home/d0np0br3/Music/downloads/tagtest/Moby feat. Kelli Scarr - Wait for Me.mp3
+/home/d0np0br3/Music/downloads/Mono - Life In Mono.mp3
+/home/d0np0br3/Music/downloads/Paul van Dyk, Arty, Sue McLaren - Sun After Heartbreak (Escape Mix).mp3
+/home/d0np0br3/Music/downloads/Rayan Myers feat.Evi Vokaly - My Everything (ATB Cover).mp3
+/home/d0np0br3/Music/downloads/Schiller, Kim Sanders - Distance.mp3
+/home/d0np0br3/Music/downloads/Schiller, Kim Sanders - Delicately Yours.mp3
+/home/d0np0br3/Music/downloads/Schiller, Kim Sanders - I Saved You.mp3
+/home/d0np0br3/Music/downloads/Schiller, Kim Sanders - Dancing With Loneliness.mp3
+/home/d0np0br3/Music/downloads/Solarstone & Jes - Like A Waterfuall (Lime Chill Remix).mp3
+/home/d0np0br3/Music/downloads/Tritonal Feat. Christina Soto - Piercing Quiet.mp3
+/home/d0np0br3/Music/downloads/Madonna - Bittersweet (Rumi).mp3
+/home/d0np0br3/Music/downloads/Paul Oakenfold - Southern Sun (Solarstone Chill Out Mix).mp3
+/home/d0np0br3/Music/downloads/Miro - By Your Side (Rolled mix).mp3
+/home/d0np0br3/Music/downloads/Liquid State - Falling (feat. Marcella Woods, Solarstone Afterhours Mix).mp3
+/home/d0np0br3/Music/downloads/Vibrasphere - Ensueno (Ambient).mp3
